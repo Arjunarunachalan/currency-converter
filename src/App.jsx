ff157c75f8df0142c43118e9b834784ff08f20fb
arjun <arjunarunachalan@gmail.com>
2024-04-07 11:33:09 +0530

    conversion works perfectly

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import "./App.css";
 import Currency_converter from "./components/Currency_converter";
 
 function App() {
-
   https: return (
     <div>
-      <h1 className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-        <Currency_converter />
-      </h1>
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center ">
+        <div className="container">
+          <Currency_converter />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Currency_converter.jsx
+++ src/components/Currency_converter.jsx
@@ -7,6 +7,9 @@ const Currency_converter = () => {
   const [amount, setAmount] = useState(1);
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+  const [converting, setConverting] = useState(false);
+
   const fetchCurrency = async () => {
     try {
       const res = await fetch("https://api.frankfurter.app/currencies");
@@ -20,18 +23,32 @@ const Currency_converter = () => {
   useEffect(() => {
     fetchCurrency();
   }, []);
-  console.log(currencies);
-  const convertCurrency = () => {};
+
+  const convertCurrency = async () => {
+    if (!amount) return;
+    setConverting(true);
+    try {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+      setConvertedAmount(data.rates[toCurrency] + " " + toCurrency);
+    } catch (error) {
+      console.error("Error Fetching", error);
+    } finally {
+      setConverting(false);
+    }
+  };
+
   const handlefavorite = (currency) => {
     console.log("handle favorite");
   };
 
   const swapCurrencies = () => {
     setFromCurrency(toCurrency);
-    setToCurrency(fromCurrency)
+    setToCurrency(fromCurrency);
   };
 
-  // https://api.frankfurter.app/latest?amount=1&from=USD&to=INR
   return (
     <div className="max-w-xl mx-auto my-10 bg-white rounded-md shadow-md p-5">
       <h1 className="text-2xl font-semibold text-gray-700 mb-10">
@@ -61,14 +78,14 @@ const Currency_converter = () => {
       </div>
       <div className="mt-4">
         <label
-          value={amount}
           htmlFor="amount"
           className="text-gray-700 font-medium text-sm block"
-          onChange={(e) => SetAmount(e.target.value)}
         >
           Amount :
         </label>
         <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
           type="number"
           className="w-full p-2 border border-gary-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
@@ -76,15 +93,19 @@ const Currency_converter = () => {
       <div className="flex justify-end mt-4">
         <button
           onClick={convertCurrency}
-          className="px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 
-        focus:outline-none focus:ring-2 focus-ring-indigo-500 focus:ring-offset-2"
+          className={`px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 
+        focus:outline-none focus:ring-2 focus-ring-indigo-500 focus:ring-offset-2 ${
+          converting ? "animate-ping" : " "
+        }`}
         >
           Convert
         </button>
       </div>
-      <div className="mt-4 text-lg text-right text-green-600 font-medium">
-        $69{" "}
-      </div>
+      {convertedAmount && (
+        <div className="mt-4 text-lg text-right text-green-600 font-medium">
+          Converted Amount : {convertedAmount}
+        </div>
+      )}
     </div>
   );
 };
